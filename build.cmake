4e6f47a857e9a165985c5868b89df67221868e7e
yutakakinjyo <yutakakinjyo@gmail.com>
2015-11-09 12:49:31 +0000

    add build script

---
+++ build.cmake
@@ -0,0 +1,28 @@
+cmake_minimum_required(VERSION 2.8)
+
+set(build_dir ${CMAKE_CURRENT_LIST_DIR}/build)
+
+if(NOT EXISTS ${build_dir})
+  file(MAKE_DIRECTORY ${build_dir})
+endif()
+
+execute_process(
+  COMMAND ${CMAKE_COMMAND} .. -Dtest=ON
+  WORKING_DIRECTORY ${build_dir}
+)
+
+execute_process(
+  COMMAND make
+  WORKING_DIRECTORY ${build_dir}
+)
+
+
+execute_process(
+  COMMAND make
+  WORKING_DIRECTORY ${build_dir}
+)
+
+execute_process(
+  COMMAND make test
+  WORKING_DIRECTORY ${build_dir}
+)
