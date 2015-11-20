eae8dbb1986d20bab1d03221d056f53853175a82
yutakakinjyo <yutakakinjyo@gmail.com>
2015-11-20 23:56:20 +0900

    fix spec for circleci

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
+# Workaround needed for circleCI
+if ENV['CIRCLECI']
+  class Docker::Container
+    def remove(options={}); end
+    alias_method :delete, :remove
+  end
+end
+
 describe package('cmake') do
   it { should be_installed }
 end
